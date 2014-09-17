c0862ca50fd0614f3a2e1e0291b29af22a9e044e
Didier Chalon <chalon.didier@skynet.be>
2014-09-17 20:56:59 +0200

    rprog-007 assignment 2 submit 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,21 +2,19 @@
 ##
 ## usage: m <- makeCacheMatrix(m1)
 ##  where m1 (optional) is a matrix
-##   an error is returned if m is not a square matrix (since only square matrices can be inverted)
+##   an error is returned if m1 is not a square matrix (since only square matrices can be inverted)
 ## this done, the following functions are available:
 ##  retrieve the contents of m: m$get()
 ##  change the contents of m to matrix m2: m$set(m2))
-##   ?? the new contents is returned
 ##   this causes the cached inverse to be reset
 ##  set the cached inverse of m to minv: m$setinverse(minv)
-##   ?? the cached inverse is returned
 ##   this function does not check whether the minv is indeed the proper inverse of m
 ##  get the cached inverse of m: m$getinverse()
 
 makeCacheMatrix <- function(x = matrix()) {
         ## makeCacheMatrix allows to define and manage matrices with the possibility to cache their inverse
                 
-        ## if a value is provided for x, check that it is a matrix indeed
+        ## if a value is provided for x, check that it is a square matrix indeed
         if (!is.null(x)) {
                 stopifnot(is.matrix(x),nrow(x)==ncol(x))
         }
@@ -61,7 +59,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ##
 ## usage: cacheSolve(m)
 ##  this returns the inverse of m
-##  relying on R to stop with various messages when m has not been created with makeCacheMatrix
+##  relying on R to stop with various messages when m has not been created with makeCacheMatrix hence no parameter type checking
 ##  if m already has its inverse cached (i.e. m$getinverse() returns something) then the cached value is returned
 ##  if m doesn't have its inverse cached yet (i.e. m$getinverse() returns NULL) then the inverse is computed, cached and returned
 
@@ -69,7 +67,7 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         ##  relying on R to stop with various messages when x has not been created with makeCacheMatrix 
-        ##   hence so no parameter type checking
+        ##   hence no parameter type checking
                 
         ## check if inverse has already been cached
         minv <- x$getinverse()
@@ -81,7 +79,7 @@ cacheSolve <- function(x, ...) {
         }
         
         ## else retrieve the contents of x and compute its inverse
-        ##  note that x is assumed to be invertiblen, hence no explicit error handling
+        ##  note that x is assumed to be invertible, hence no explicit error handling
         ##  any extra arguments '...' are passed through to the solve call
         data <- x$get()
         minv <- solve(data, ...)
